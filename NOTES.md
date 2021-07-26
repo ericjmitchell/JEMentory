dd88bd833a1d6bb23dc27c482ce2761edb79eca3
Eric Mitchell <ericmitchell42@gmail.com>
2021-07-25 18:47:32 -0600

    Set up inventory table

---
+++ NOTES.md
@@ -0,0 +1,15 @@
+# Notes
+
+## Access Patterns
+
+Get all items for family
+Get all users for family
+Crud Item
+Get family for user
+
+
+## Database Design
+USER:ID PROFILE email phash familyId name(first)
+FAMILY:ID PROFILE name(surname)
+          USER#ID
+          ITEM#ID
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const containers = require('./containers')
+//const containers = require('./containers')
 const items = require('./items')
-const rooms = require('./rooms')
+//const rooms = require('./rooms')
 
-router.use('/containers', containers)
+//router.use('/containers', containers)
 router.use('/items', items)
-router.use('/rooms', rooms)
+//router.use('/rooms', rooms)
 
 module.exports = router
