34a41bda2d0ace9b26653289d133227b8ee91272
Eric Mitchell <ericmitchell42@gmail.com>
2021-08-08 15:35:36 -0400

    Remove containers and rooms

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-//const containers = require('./containers')
 const items = require('./items')
-//const rooms = require('./rooms')
 
-//router.use('/containers', containers)
 router.use('/items', items)
-//router.use('/rooms', rooms)
 
 module.exports = router
