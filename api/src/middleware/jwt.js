fa42670fa57e2aaba19c3529b6af7cdd6f2c874d
Eric Mitchell <ericmitchell42@gmail.com>
2021-08-10 21:32:28 -0600

    Finished authentication

--- api/src/middleware/jwt.js
+++ api/src/middleware/jwt.js
@@ -1,12 +1,12 @@
 const expressJwt = require('express-jwt');
-const config = require('config.json');
 
 function jwt() {
     const secret = process.env.secret;
     return expressJwt({ secret, algorithms: ['HS256'] }).unless({
         path: [
             // public routes that don't require authentication
-            '/users/authenticate'
+            '/users/authenticate',
+            '/users/'
         ]
     });
 }
